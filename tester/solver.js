ee458316a8d7ad03ccca3857fadb65dd4c4a12bc
simone-cartalemi <scsimocarta@gmail.com>
2024-04-18 14:02:12 +0200

    feat: added web page example (websocket)

---
+++ tester/solver.js
@@ -0,0 +1,51 @@
+const address = "localhost";
+const port = 3264;
+
+document.addEventListener("DOMContentLoaded", (event) => {
+
+	const inputMatrix = document.getElementById('matrix').value;
+	const send = document.getElementById('send');
+	const responses = document.getElementById('responses');
+	const outp = (message) => {
+		responses.innerHTML += "<p>" + message + "</p>";
+	}
+
+	const connect = (_address, _port, data, fun) => {
+		socket = new WebSocket("ws:" + _address + ":" + _port);
+		socket.addEventListener("open", () => {
+			console.log('Connesso al server');
+			outp("Ok");
+			socket.send(data);
+		});
+		socket.addEventListener("message", (data) => {
+			console.log('Messaggio:', data);
+			outp("Risultato: " + data);
+			fun(data);
+		});
+		socket.addEventListener("close", () => {
+			console.log('Connessione chiusa');
+			outp("Closed");
+		});
+		socket.addEventListener("error", (error) => {
+			console.error('Errore durante la connessione al server: ', error);
+			outp("Error");
+		});
+		return socket;
+	};
+	
+
+
+	send.addEventListener('click', () => {
+		matrix = inputMatrix.value;
+		const sock = connect(address, port, matrix, (response) => {
+			outp(response);
+		});
+		sock.send("CIAO")
+		outp("Loading...");
+	});
+
+
+
+
+
+});
