382df8d21befac78f7fe8401c59e7131c05da5d9
simone-cartalemi <scsimocarta@gmail.com>
2024-05-05 12:11:22 +0200

    test: first success solving

--- solver-server-image/main.py
+++ solver-server-image/main.py
@@ -3,13 +3,28 @@ import asyncio
 import websockets
 import threading
 
+SERVER_PORT = 3264
+SOLVER_PORT = 1632
 
-def send_command_and_receive_file(host, port, command):
+
+def check_matrix(matrix):
+    if not matrix.startswith("[") or not matrix.endswith("]"):
+        return False
+    t = matrix[1:-1]
+    elements = t.split(",")
+    if len(elements) != 81:
+        return False
+    for e in elements:
+        if int(e) < 0 or int(e) > 9:
+            return False
+    return True
+
+def solve_matrix(host, matrix):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
-        sock.connect((host, port))
-        sock.sendall(command.encode() + b'\n')
+        sock.connect((host, SOLVER_PORT))
+        sock.sendall(matrix.encode() + b'\n')
         
         # Ricevi la dimensione del file dal server
         file_size = 1024
@@ -22,60 +37,48 @@ def send_command_and_receive_file(host, port, command):
                 break
             received_data += data
         
-        # Salva il file ricevuto
-        with open('results/res.txt', 'w+b') as f:
-            f.write(received_data)
-            print("File salvato come 'output_file'")
+        # Invia la soluzione ricevuta
+        return received_data
 
     finally:
         sock.close()
 
-
-def oo_main():
-    print("OK CI SIAMO\n\n\n\n\n")
-    for i in range(10):
-        print("Tentativo", i + 1)
-        m = "[8,0,0,3,0,5,0,4,2,2,6,0,7,0,9,0,3,0,1,0,3,4,2,6,9,0,8,0,2,0,0,5,0,8,0,7,0,3,0,9,7,0,0,1,0,7,8,9,0,0,2,3,6,5,6,0,5,0,0,7,1,0,0,0,7,0,2,3,0,0,5,0,0,1,2,0,6,0,7,8,9]"
-        worker_service_url = "multi-solver-service"
-        ip_solver = socket.gethostbyname(worker_service_url)
-        response = send_command_and_receive_file(ip_solver, 3264, m)
-        print(response, "risolto da ", ip_solver)
-        time.sleep(1)
-    time.sleep(5000)
-
-
-
-
-
-# --------------
 async def handle_client(websocket, path):
     async for message in websocket:
-        print(f"Received message from client: {message}")
-        
-        # Avvia un thread per eseguire la richiesta del client
         threading.Thread(target=process, args=(websocket, message)).start()
 
-def process(websocket, message):
-    asyncio.run(solve_sudoku(websocket, message))
+def process(websocket, matrix):
+    asyncio.run(solve_sudoku(websocket, matrix))
 
-async def solve_sudoku(websocket, message):
-    print(f"Processed message: {message}")
+async def solve_sudoku(websocket, matrix):
+    # Controlla il formato
+    if not check_matrix(matrix):
+        await websocket.send("Wrong format")
 
-    #worker_service_url = "multi-solver-service"
-    #ip_solver = socket.gethostbyname(worker_service_url)
-    #response = send_command_and_receive_file(ip_solver, 3264, message)
-    response = f"CIAO E {message}"
+    # Ottieni l'indirizzo del solver e risolvi
+    print(f"Input matrix: {matrix}")
+    worker_service_url = "multi-solver-service"
+    ip_solver = socket.gethostbyname(worker_service_url)
+    print(ip_solver)
+    solution = solve_matrix(ip_solver, matrix)
     
     # Invia la soluzione al client
-    await websocket.send(response)
+    if solution != None:
+        await websocket.send(solution)
+
 
 async def main():
-    async with websockets.serve(handle_client, "0.0.0.0", 3264):
-        print("WebSocket server is running...")
+    # Ascolta su tutte le interfacce
+    async with websockets.serve(handle_client, "0.0.0.0", SERVER_PORT):
+        print("WebSocket Solver server is running...")
         
         await asyncio.Future()
 
 
 # Avvio del server
 if __name__ == "__main__":
+    print("Tentativo")
+    worker_service_url = "multi-solver-service"
+    ip_solver = socket.gethostbyname(worker_service_url)
+    print(ip_solver)
     asyncio.run(main())
